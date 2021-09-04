f3ea0dc5f3bc2ed734754eba5f8630e6c726f2a0
Vyacheslav Tretyakov <tretiakovslava@gmail.com>
2021-09-05 00:43:57 +0500

    Initial commit

---
+++ src/test/kotlin/com/weblabs/musiclib/MusiclibApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.weblabs.musiclib
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MusiclibApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
